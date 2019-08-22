cc97f6d0fa81ca233f4cb39e66dc895e32eb968e
Johan Nemitz <jnemitz@cisco.com>
2019-08-21 20:14:08 -0400

    Added support for variables in day-0 and templates
    Signed-off-by: Johan Nemitz <jnemitz@cisco.com>

--- python/pnp_manager/main.py
+++ python/pnp_manager/main.py
@@ -1,5 +1,7 @@
 # -*- mode: python; python-indent: 4 -*-
+import _ncs
 import ncs
+import ncs.maapi as maapi
 from ncs.application import Service
 
 
@@ -10,28 +12,42 @@ class PnPDevice(Service):
 
         vars = ncs.template.Variables()
         template = ncs.template.Template(service)
-        for rolename in service.role:
-            # This will use the last role with PnP infor in the devices roles.
+        for servicerole in service.role:
+            # This will use the last role with PnP info in the devices roles.
             # Need to modify the service model to restrict to only one role
-            # with PnP info 
-            role = root.device_role[rolename]
-            if role.pnp.authgroup is not None:
-                vars.add('AUTHGROUP', role.pnp.authgroup)
-            if role.pnp.username is not None:
-                vars.add('USERNAME', role.pnp.username)
-            if role.pnp.port is not None:
-                vars.add('PORT', role.pnp.port)
-            if role.pnp.day0_file is not None:
-                vars.add('DAY0-FILE', role.pnp.day0_file)
+            # with PnP info
+            role = root.device_role[servicerole.name]
+            if role.pnp.authgroup and role.pnp.day0_file:
+                self.log.info("Processing role: "+servicerole.name)
+                if role.pnp.authgroup is not None:
+                    vars.add('AUTHGROUP', role.pnp.authgroup)
+                    if root.devices.authgroups.group[role.pnp.authgroup].default_map.remote_name:
+                        vars.add('USERNAME', root.devices.authgroups.group[role.pnp.authgroup].default_map.remote_name)
+                    if root.devices.authgroups.group[role.pnp.authgroup].default_map.remote_password:
+                        vars.add('PASSWORD', decrypt(root.devices.authgroups.group[role.pnp.authgroup].default_map.remote_password))
+                if role.pnp.username is not None:
+                    vars.add('USERNAME', role.pnp.username)
+                if role.pnp.password is not None:
+                    vars.add('PASSWORD', role.pnp.password)
+                if role.pnp.port is not None:
+                    vars.add('PORT', role.pnp.port)
+                if role.pnp.day0_file is not None:
+                    vars.add('DAY0-FILE', role.pnp.day0_file)
 
-        if service.authgroup is not None:
-            vars.add('AUTHGROUP', service.authgroup)
-        if service.username is not None:
-            vars.add('USERNAME', service.username)
-        if service.port is not None:
-            vars.add('PORT', service.port)
-        if service.day0_file is not None:
-            vars.add('DAY0-FILE', service.day0_file)
+        # if service.authgroup is not None:
+        #     vars.add('AUTHGROUP', service.authgroup)
+        #     if root.devices.authgroups.group[service.authgroup].default_map.remote_name:
+        #         vars.add('USERNAME', root.devices.authgroups.group[service.authgroup].default_map.remote_name)
+        #     if root.devices.authgroups.group[service.authgroup].default_map.remote_password:
+        #         vars.add('PASSWORD', decrypt(root.devices.authgroups.group[service.authgroup].default_map.remote_password))
+        # if service.username is not None:
+        #     vars.add('USERNAME', service.username)
+        # if service.password is not None:
+        #     vars.add('PASSWORD', service.password)
+        # if service.port is not None:
+        #     vars.add('PORT', service.port)
+        # if service.day0_file is not None:
+        #     vars.add('DAY0-FILE', service.day0_file)
 
         template.apply('pnp-manager-device-pnp-map', vars)
 
@@ -42,3 +58,8 @@ class Main(ncs.application.Application):
 
     def teardown(self):
         self.log.info('Main FINISHED')
+
+def decrypt(value):
+    with maapi.Maapi() as m:
+        m.install_crypto_keys()
+    return _ncs.decrypt(value)
